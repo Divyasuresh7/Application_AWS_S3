ccacb5d9a710f9398660186774cd7a76ea7d3ffb
Divyasuresh7 <93040292+Divyasuresh7@users.noreply.github.com>
2023-07-28 12:40:16 +0530

    Removed code for connection with AD- testing against one user only

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 from flask import Flask, request, jsonify, render_template
-from ldap3 import Server, Connection, ALL
+# from ldap3 import Server, Connection, ALL
 
 app = Flask(__name__)
 
-AD_SERVER = 'OTMUMBDCPRD.orient.com'
-AD_PORT = 389  # The default LDAP port for AD is 389
-AD_BASE_DN = 'DC=orient,DC=com'  
+# AD_SERVER = ''
+# AD_PORT = 389  # The default LDAP port for AD is 389
+# AD_BASE_DN = 'DC=orient,DC=com'  
+
+# def validate_credentials(username, password):
+#     try:
+#         # Creating a connection to the Active Directory server
+#         server = Server(AD_SERVER, port=AD_PORT, get_info=ALL)
+#         conn = Connection(server, user=f"{username}@orient.com", password=password)
+
+#         # Binding to the server to check the credentials
+#         if conn.bind():
+#             return True
+#         else:
+#             return False
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         return False
+
+VALID_USERNAME = 'Admin'
+VALID_PASSWORD = 'Admin@123'
 
 def validate_credentials(username, password):
-    try:
-        # Creating a connection to the Active Directory server
-        server = Server(AD_SERVER, port=AD_PORT, get_info=ALL)
-        conn = Connection(server, user=f"{username}@orient.com", password=password)
-
-        # Binding to the server to check the credentials
-        if conn.bind():
-            return True
-        else:
-            return False
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
+    return username == VALID_USERNAME and password == VALID_PASSWORD
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('new.html')
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
+    data = request.form  # Get the form data submitted from login.html
+    username = data.get('username')
+    password = data.get('password')
 
     if username and password:
         if validate_credentials(username, password):
@@ -39,10 +46,11 @@ def login():
 
     return jsonify({'success': False, 'message': 'Username and password are required'})
 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
+
